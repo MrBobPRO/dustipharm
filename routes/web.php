a468afa6110fc77ff3977194919c9bded5fadc8a
MrBobPRO <bobnuridinov@gmail.com>
2023-01-16 14:41:13 +0500

    Installed Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [MainController::class, 'home']);
+Route::get('/', [MainController::class, 'home'])->name('home');
+
+require __DIR__.'/auth.php';
