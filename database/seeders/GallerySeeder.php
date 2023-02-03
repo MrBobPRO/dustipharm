292597802ae8f19210a17280099b450139fe778e
MrBobPRO <bobnuridinov@gmail.com>
2023-02-03 15:31:03 +0500

    Gallery seeder updated

--- database/seeders/GallerySeeder.php
+++ database/seeders/GallerySeeder.php
@@ -15,8 +15,8 @@ class GallerySeeder extends Seeder
    */
   public function run()
   {
-    $filename = ['63dca73c2ca85.png', '63dca15422a66.png', '63dca15425ca3.png', '63dca6506581e.png'];
-    $drugstore_id = [1,2,3,4];
+    $filename = ['63dca73c2ca85.png', '63dca15422a66.png', '63dca15425ca3.png', '63dca6506581e.png', '63dcdf174ee24.png', '63dce0ed4b590.png', '63dce0f34d108.png', '63dce0f84bf90.png'];
+    $drugstore_id = [1,2,3,4,1,2,3,4];
 
     for ($i=0; $i<count($filename); $i++)
     {
